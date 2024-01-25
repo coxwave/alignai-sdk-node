e8ef7ab9f469e79e11164200f57091b2fe40cc5a
Dongeon Kim <kde713@gmail.com>
2024-01-25 20:32:02 +0900

    feat: Add Web Transport Option

--- src/client.ts
+++ src/client.ts
@@ -1,20 +1,36 @@
-import {createPromiseClient, PromiseClient} from "@connectrpc/connect";
-import {createConnectTransport} from "@connectrpc/connect-node";
+import {createPromiseClient, PromiseClient, Transport} from "@connectrpc/connect";
+import {createConnectTransport as createNodeConnectTransport} from "@connectrpc/connect-node";
+import {createConnectTransport as createWebConnectTransport} from "@connectrpc/connect-web";
 import {v4 as uuidv4} from 'uuid';
 import {defaultApiHost} from "./consts";
 import {Event} from "./events";
 import {IngestionService} from "@buf/impaction-ai_ingestion.connectrpc_es/ingestion/v1alpha/ingestion_connect";
+
+export enum TransportType {
+    NodeHTTP11,
+    Web,
+}
 
 export class AlignAI {
     private readonly projectId: string;
     private readonly apiKey: string;
     private client: PromiseClient<typeof IngestionService>;
 
-    constructor(projectId?: string, apiKey?: string, apiHost?: string) {
-        const transport = createConnectTransport({
-            httpVersion: "1.1",
-            baseUrl: apiHost ?? defaultApiHost,
-        });
+    constructor(projectId?: string, apiKey?: string, apiHost?: string, transportType: TransportType = TransportType.NodeHTTP11) {
+        let transport: Transport;
+        switch (transportType) {
+            case TransportType.NodeHTTP11:
+                transport = createNodeConnectTransport({
+                    httpVersion: "1.1",
+                    baseUrl: apiHost ?? defaultApiHost,
+                });
+                break;
+            case TransportType.Web:
+                transport = createWebConnectTransport({
+                    baseUrl: apiHost ?? defaultApiHost,
+                });
+                break;
+        }
         this.projectId = projectId ?? process.env.ALIGNAI_PROJECT_ID ?? '';
         this.apiKey = apiKey ?? process.env.ALIGNAI_API_KEY ?? '';
         this.client = createPromiseClient(IngestionService, transport);
