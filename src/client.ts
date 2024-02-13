71e69c2984ca68fb036bccc5e080a067d5de2e5f
Dongeon Kim <kde713@gmail.com>
2024-02-13 23:00:59 +0900

    Remove code dep

--- src/client.ts
+++ src/client.ts
@@ -1,36 +1,20 @@
-import {createPromiseClient, PromiseClient, Transport} from "@connectrpc/connect";
+import {createPromiseClient, PromiseClient} from "@connectrpc/connect";
 import {createConnectTransport as createNodeConnectTransport} from "@connectrpc/connect-node";
-import {createConnectTransport as createWebConnectTransport} from "@connectrpc/connect-web";
 import {v4 as uuidv4} from 'uuid';
 import {defaultApiHost} from "./consts";
 import {Event} from "./events";
 import {IngestionService} from "./buf/ingestion_connect";
-
-export enum TransportType {
-    NodeHTTP11,
-    Web,
-}
 
 export class AlignAI {
     private readonly projectId: string;
     private readonly apiKey: string;
     private client: PromiseClient<typeof IngestionService>;
 
-    constructor(projectId?: string, apiKey?: string, apiHost?: string, transportType: TransportType = TransportType.NodeHTTP11) {
-        let transport: Transport;
-        switch (transportType) {
-            case TransportType.NodeHTTP11:
-                transport = createNodeConnectTransport({
-                    httpVersion: "1.1",
-                    baseUrl: apiHost ?? defaultApiHost,
-                });
-                break;
-            case TransportType.Web:
-                transport = createWebConnectTransport({
-                    baseUrl: apiHost ?? defaultApiHost,
-                });
-                break;
-        }
+    constructor(projectId?: string, apiKey?: string, apiHost?: string) {
+        const transport = createNodeConnectTransport({
+            httpVersion: "1.1",
+            baseUrl: apiHost ?? defaultApiHost,
+        });
         this.projectId = projectId ?? process.env.ALIGNAI_PROJECT_ID ?? '';
         this.apiKey = apiKey ?? process.env.ALIGNAI_API_KEY ?? '';
         this.client = createPromiseClient(IngestionService, transport);
